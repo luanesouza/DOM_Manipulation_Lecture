34a8062fa554cf82394eb4a386f1a013fdc178cf
luanesouza <annie.usa25@gmail.com>
2019-08-15 13:23:20 -0400

    adds fetch

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 console.log("js listening");
-console.log(API_KEY);
+
+const url = `http://api.giphy.com/v1/gifs/search?q=`
 
 
 let gif_box = document.querySelector(".gif_box");
@@ -12,8 +13,17 @@ let p = document.createElement('p');
 form.addEventListener("submit", function(event){
   event.preventDefault();
   p.innerText = input.value;
-
-
+  fetchingData()
 })
-
 section.append(p);
+
+
+const fetchingData = (data) => {
+  let userInput = p.innerText
+  fetch(`${url}${userInput}&api_key=${API_KEY}&limit=5}`, {
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  }).then(response => console.log(response))
+}
+// fetchingData();
